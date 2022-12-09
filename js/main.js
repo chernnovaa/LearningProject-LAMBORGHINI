7c960f658d847954a53ef31c64c4695520bb1aa9
Chernova Kateryna <katac0747@gmail.com>
2022-12-09 16:34:25 +0200

    fix bugs

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   loop: true,
-
+  slidesPerView: 2,
+  spaceBetween: 20,
   // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-right",
+    prevEl: ".swiper-button-left",
   },
 });
